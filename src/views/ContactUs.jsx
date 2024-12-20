d2dcbd72616893213a7d66ead5633d99ba14e139
Saleheen01 <153016056+Saleheen01@users.noreply.github.com>
2024-12-20 22:21:22 +0600

    ContactUs

---
+++ src/views/ContactUs.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+export default function ContactUs() {
+    return (
+      <section className="text-center mt-4">
+        <h2>Contact Us</h2>
+        
+      </section>
+    );
+  }
