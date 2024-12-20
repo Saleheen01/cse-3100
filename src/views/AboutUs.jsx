4961c3e14199a7ae6341b7019c2b48019e307c17
Saleheen01 <153016056+Saleheen01@users.noreply.github.com>
2024-12-20 23:56:23 +0600

    aboutus problem solve

---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+export default function AboutUs() {
+    return (
+      <section className="text-center mt-4">
+        <h2>About Us</h2>
+        <p>
+          Welcome to Purrfect Adoption! Our mission is to help cats find their forever homes. We believe every cat deserves a loving family and are dedicated
+          to matching our furry friends with the right adopters. Join us in making a difference!
+        </p>
+      </section>
+    );
+  }
